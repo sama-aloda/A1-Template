135ce1945789b057298d45496613b13bac28d360
sama-aloda <alodas@mcmaster.ca>
2025-02-03 14:31:04 -0500

    Added Canonical and Factorized Closes #10 Closes #9

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -1,14 +1,10 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 
-public abstract class Path{
-    Maze maze;
-    String path;
-    Player player;
+public abstract class Path implements PathRequirements{
+    protected Maze maze;
+    protected String path;
+    protected Player player;
 
     public Path(){
         maze=new Maze();
@@ -16,91 +12,161 @@ public abstract class Path{
         player=new Player();
     }
 
+    @Override
     public String getPath(){
         return path;
     }
 
+    @Override
     public Maze getMaze(){
         return maze;
     }
 
-    public String pathParser(String p){
+    public String getCanonicalfromFactorized(String p){
+        if(p.length()<=0)
+            return "";
+        int i=0;
+        String n="";
+        while(i<p.length()-1){
+            if(p.charAt(i)==' '){
+                i++;
+            }
+            else{
+            try{
+                int j= Integer.parseInt(p.substring(i,i+1));
+                int z=0;
+                while(z<j-1){
+                    n+=p.charAt(i+1);
+                    z++;
+                }
+                i++;
+            }catch(NumberFormatException e){
+                n+=p.charAt(i);
+                i++;
+            }
+            }
+        }
+        n+=p.charAt(i);
+        return n;
+    }
+    @Override
+    public String getCanonicalForm(String p){
         int r=0;
         int f=0;
         int i=0;
+        int l=0;
+        String newStr = "";
+        while(i<p.length()){
+            if(i==0){
+                if(p.charAt(i)=='R')
+                    newStr+="R";
+                else if (p.charAt(i)=='F')
+                    newStr+="F";
+                else
+                    newStr+="L";
+            }
+            else{
+                if(p.charAt(i-1)==p.charAt(i)){
+                    if(p.charAt(i)=='R')
+                        newStr+="R";
+                    else if (p.charAt(i)=='F')
+                        newStr+="F";
+                    else
+                        newStr+="L";
+                }
+                else {
+                    if(f==0&&l==0&&r!=1)
+                        newStr+=" ";
+                    else if (r==0&&l==0&&f!=1)
+                        newStr+=" ";
+                    else if(l!=1&&r==0&&f==0)
+                        newStr+=" ";
+                    else if(r==1)
+                        newStr+=" ";
+                    else if (f==1)
+                        newStr+=" ";
+                    else
+                        newStr+=" ";
+                    f=0;
+                    r=0;
+                    l=0;
+                    if(p.charAt(i)=='R')
+                        newStr+="R";
+                    else if (p.charAt(i)=='F')
+                        newStr+="F";
+                    else
+                        newStr+="L";
+                }
+            }
+            i++;
+        }
+        if(f==0&&l==0&&r!=0)
+            newStr+=r+"R";
+        else if(r==0&&l==0&&f!=0) 
+            newStr+=f+"F";
+        else if(l!=0)
+            newStr+=l+"L";
+        return newStr;
+    }
+
+    @Override
+    public String getFactorizedForm(String p){
+        int r=0;
+        int f=0;
+        int i=0;
+        int l=0;
         String newStr = "";
         while(i<p.length()){
             if(i==0){
                 if(p.charAt(i)=='R')
                     r++;
-                else
+                else if (p.charAt(i)=='F')
                     f++;
+                else
+                    l++;
             }
             else{
                 if(p.charAt(i-1)==p.charAt(i)){
                     if(p.charAt(i)=='R')
                         r++;
-                    else
+                    else if (p.charAt(i)=='F')
                         f++;
+                    else
+                        l++;
                 }
                 else {
-                    if(f==0)
-                        newStr+=r+"R";
+                    if(f==0&&l==0&&r!=1)
+                        newStr+=r+"R ";
+                    else if (r==0&&l==0&&f!=1)
+                        newStr+=f+"F ";
+                    else if(l!=1&&r==0&&f==0)
+                        newStr+=l+"L ";
+                    else if(r==1)
+                        newStr+="R ";
+                    else if (f==1)
+                        newStr+="F ";
                     else
-                        newStr+=f+"F";
+                        newStr+="L ";
                     f=0;
                     r=0;
+                    l=0;
                     if(p.charAt(i)=='R')
                         r++;
-                    else
+                    else if (p.charAt(i)=='F')
                         f++;
+                    else
+                        l++;
                 }
             }
             i++;
         }
-        if(f==0)
+        if(f==0&&l==0)
             newStr+=r+"R";
-        else
+        else if(r==0&&l==0) 
             newStr+=f+"F";
+        else
+            newStr+=l+"L";
         return newStr;
     }
 }
 
-class PathFinder extends Path{
-
-    public PathFinder(){
-        super();
-    }
-
-    public void rightHand(){
-        String actions="";
-        for(int i=0;i<maze.getMaze().size();i++){ //find entry point of maze
-            if(maze.getSpot(i,0).getValue()==' '){
-                player.setSpot(new int[] {i,0});
-                player.setAngle(0);
-            }
-        }
-        
-        System.out.println("Entry spot is at: "+player.getSpot()[0]+" "+player.getSpot()[1]);
-
-
-        for(int i=0;i<maze.getMaze().get(0).size();i++){
-            int[] toRight= player.checkRight();
-            if(maze.getSpot(toRight[0],toRight[1]).getValue()=='#'){
-                player.forward(); 
-            }
-            else{
-                player.forward();
-                player.right();
-                path+="R";
-            }
-            path+="F";
-            System.out.println(path+" is current path of the player.");
-            System.out.println("Current Spot: "+player.getSpot()[0]+" "+player.getSpot()[1]);
-
-        }   
-        System.out.println("\n"+path+" is the final path.");
-        System.out.println(super.pathParser(path)+" is the path parsed.");
-        
-    }
-}
